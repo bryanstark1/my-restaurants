577c0f0e89042d84c325d5d0bd4ec2df4d213828
bryanstark1 <bryandstark@icloud.com>
2023-07-10 15:54:02 -0400

    Added login page and functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -7,6 +8,7 @@ class Restaurant(models.Model):
     location = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
